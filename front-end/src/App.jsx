f95a70af62cf19c085cbf79eb7522afe4ad42f99
Pranava Pai <pranavpai.sahyadri@gmail.com>
2026-02-03 16:59:08 +0530

    Feat : Added new Routes

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,6 +1,8 @@
 import {BrowserRouter as Router, Routes , Route} from "react-router-dom"
 import Home from './pages/Home.jsx'
-import Search from "./pages/Search.jsx"
+import Search from "./pages/Search.jsx";
+import Generate from "./pages/Generate.jsx";
+import Analytics from "./pages/Analytics.jsx";
 import './App.css'
 
 function App() {
@@ -13,6 +15,8 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path="/search" element={<Search/>}/>
+        <Route path="/generate" element={<Generate/>}/>
+        <Route path="/analytics" element={<Analytics/>}/>
       </Routes>
       </Router>
       </div>
